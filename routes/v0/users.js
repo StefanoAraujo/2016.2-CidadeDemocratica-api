c7da1af8076d83ed0e3dee38074b29708e3daccf
thiagotmb <tmb0710@gmail.com>
2016-09-18 20:24:43 -0300

    Fixing users query to complains with data access

--- routes/v0/users.js
+++ routes/v0/users.js
@@ -5,7 +5,7 @@ var db = require('../../config/db.js');
 router.route('/users')
 .get(function(req,res) {
 
-  db.mysqlConnection.query('SELECT * FROM users INNER JOIN user_dados ON users.id = user_dados.user_id', function(err, rows, fields) {
+  db.mysqlConnection.query('SELECT user_dados.user_id, user_dados.nome, user_dados.descricao, user_dados.sexo, user_dados.aniversario, users.id, users.state, users.type, users.topicos_count, users.comments_count, users.adesoes_count, users.relevancia, users.inspirations_count FROM users INNER JOIN user_dados ON users.id = user_dados.user_id', function(err, rows, fields) {
     if (!err){
       res.json(rows);
     }else{
@@ -21,7 +21,7 @@ router.route('/users/:user_id')
   if (isNaN(req.params.user_id)) {
     return res.json("The param is not a number");
   }
-  var sqlQueryString = "SELECT * FROM users INNER JOIN user_dados ON users.id = user_dados.user_id WHERE users.id = " + req.params.user_id;
+  var sqlQueryString = "SELECT user_dados.user_id, user_dados.nome, user_dados.descricao, user_dados.sexo, user_dados.aniversario, users.id, users.state, users.type, users.topicos_count, users.comments_count, users.adesoes_count, users.relevancia, users.inspirations_count FROM users INNER JOIN user_dados ON users.id = user_dados.user_id WHERE users.id = " + req.params.user_id;
   db.mysqlConnection.query(sqlQueryString, function(err, rows, fields) {
     if (!err){
       res.json(rows);
