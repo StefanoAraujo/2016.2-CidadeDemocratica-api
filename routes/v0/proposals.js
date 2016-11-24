9be64e0fc41f49d569bda6a0e4b80fc36884d1b4
Pedro Sales <pedrohsalesf@gmail.com>
2016-11-24 19:19:23 -0200

    Get a list of all favorited proposals

--- routes/v0/proposals.js
+++ routes/v0/proposals.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var db = require('../../config/db.js');
 require('../../model/proposal')
 var mongoose = require('mongoose'),
-  Proposals = mongoose.model('Proposal');
+    Proposals = mongoose.model('Proposal');
 
 var query = 'SELECT topicos.id, topicos.user_id, topicos.titulo, topicos.descricao, topicos.slug, topicos.comments_count, topicos.adesoes_count, topicos.relevancia, topicos.seguidores_count, topicos.competition_id, topicos.site, cidades.nome AS `city_name`, estados.nome AS `state_name`, estados.abrev AS `state_abrev`FROM topicos INNER JOIN locais ON topicos.type = "Proposta" AND locais.responsavel_type = "User" AND locais.responsavel_id = topicos.id INNER JOIN cidades ON cidades.id = locais.cidade_id INNER JOIN estados ON estados.id = locais.estado_id'
 
@@ -80,58 +80,58 @@ var query = 'SELECT topicos.id, topicos.user_id, topicos.titulo, topicos.descric
  *           $ref: '#/definitions/Proposal'
  */
 router.route('/proposals')
-  .get(function (req, res) {
-    var token = req.headers.authorization
-    var result = false
-    authvalidate.isValidToken(token, function (isValid) {
-      result = isValid
-      if (result) {
-        var newQuery = query
-        var wheresCount = 0
-        // console.log()
-        if (req.query.tag_id != null && parseInt(req.query.tag_id) > 0) {
-          var tagFilterQuery = ' WHERE topicos.id in (select taggings.taggable_id from taggings where taggings.tag_id = ' + req.query.tag_id + ") "
-          newQuery = newQuery + tagFilterQuery
-          wheresCount += 1
-        }
+    .get(function(req, res) {
+        var token = req.headers.authorization
+        var result = false
+        authvalidate.isValidToken(token, function(isValid) {
+            result = isValid
+            if (result) {
+                var newQuery = query
+                var wheresCount = 0
+                // console.log()
+                if (req.query.tag_id != null && parseInt(req.query.tag_id) > 0) {
+                    var tagFilterQuery = ' WHERE topicos.id in (select taggings.taggable_id from taggings where taggings.tag_id = ' + req.query.tag_id + ") "
+                    newQuery = newQuery + tagFilterQuery
+                    wheresCount += 1
+                }
 
-        if (req.query.user_id != null && parseInt(req.query.user_id) > 0) {
-          if (wheresCount > 0) {
-            newQuery = newQuery + ' AND '
-          } else {
-            newQuery = newQuery + ' WHERE '
-          }
+                if (req.query.user_id != null && parseInt(req.query.user_id) > 0) {
+                    if (wheresCount > 0) {
+                        newQuery = newQuery + ' AND '
+                    } else {
+                        newQuery = newQuery + ' WHERE '
+                    }
 
-          newQuery = newQuery + ' topicos.user_id = ' + req.query.user_id
-        }
+                    newQuery = newQuery + ' topicos.user_id = ' + req.query.user_id
+                }
 
-        var page = req.query.page
-        var start = 0
-        var limit = 30
-        if (isNaN(page) || page == 0) {
-          newQuery = newQuery + ' ORDER BY topicos.relevancia DESC'
+                var page = req.query.page
+                var start = 0
+                var limit = 30
+                if (isNaN(page) || page == 0) {
+                    newQuery = newQuery + ' ORDER BY topicos.relevancia DESC'
 
-        } else {
-          start = (page - 1) * limit
+                } else {
+                    start = (page - 1) * limit
 
-          var limitToQuery = ' LIMIT ' + start + ',' + limit
-          newQuery = newQuery + ' ORDER BY topicos.relevancia DESC' + limitToQuery
-        }
+                    var limitToQuery = ' LIMIT ' + start + ',' + limit
+                    newQuery = newQuery + ' ORDER BY topicos.relevancia DESC' + limitToQuery
+                }
 
-        // console.log(newQuery);
+                // console.log(newQuery);
 
-        db.mysqlConnection.query(newQuery, function (err, rows, fields) {
-          if (!err) {
-            res.json(rows);
-          } else {
-            res.send(err);
-          }
-        });
-      } else {
-        res.json({ error: 'Not allowed to request' });
-      }
+                db.mysqlConnection.query(newQuery, function(err, rows, fields) {
+                    if (!err) {
+                        res.json(rows);
+                    } else {
+                        res.send(err);
+                    }
+                });
+            } else {
+                res.json({ error: 'Not allowed to request' });
+            }
+        })
     })
-  })
 
 /**
  * @swagger
@@ -155,89 +155,166 @@ router.route('/proposals')
  *           $ref: '#/definitions/Proposal'
  */
 router.route('/proposals/:proposal_id')
-  .get(function (req, res) {
-    var token = req.headers.authorization
-    var result = false
-    authvalidate.isValidToken(token, function (isValid) {
-      result = isValid
-      if (result) {
-        if (isNaN(req.params.proposal_id)) {
-          return res.json("The param is not a number");
-        }
-        var sqlQueryString = query + ' WHERE topicos.id = ' + req.params.proposal_id
-        console.log(sqlQueryString)
-
-        db.mysqlConnection.query(sqlQueryString, function (err, rows, fields) {
-          if (!err) {
-            res.json(rows);
-          } else {
-            res.send(err);
-          }
-        });
-      } else {
-        res.json({ error: 'Not allowed to request' });
-      }
-    })
-  })
-
-router.route('/favorite_proposals')
-  .post(function (req, res) {
-    var token = req.headers.authorization
-    var result = false
-    var find = false
-    authvalidate.isValidToken(token, function (isValid) {
-      result = isValid
-      if (result) {
-        var id = req.query.id
-        Proposals.findOne({ id: id }, {}, function (err, result) {
-
-          if (result != null) {
-            // console.log(result.users)
-            var message = " "
-            var users = result.users
-
-            if (users.length > 0) {
-              users.forEach(function (obj, index) {
-                console.log(index)
-                if (token == obj) {
-                  users.splice(index, 1);
-                  message = "User removed with success"
-                  find = true
-                  return
+    .get(function(req, res) {
+        var token = req.headers.authorization
+        var result = false
+        authvalidate.isValidToken(token, function(isValid) {
+            result = isValid
+            if (result) {
+                if (isNaN(req.params.proposal_id)) {
+                    return res.json("The param is not a number");
                 }
-              })
-              if (!find) {
-                users.push(token)
-                message = "User add with success"
-              }
+                var sqlQueryString = query + ' WHERE topicos.id = ' + req.params.proposal_id
+                console.log(sqlQueryString)
+
+                db.mysqlConnection.query(sqlQueryString, function(err, rows, fields) {
+                    if (!err) {
+                        res.json(rows);
+                    } else {
+                        res.send(err);
+                    }
+                });
             } else {
-              users.push(token)
-              message = "User add with success"
+                res.json({ error: 'Not allowed to request' });
             }
-            result.users = users
-            result.save(function (err) {
-              if (!err) {
-                console.log(result.users)
-                res.json({
-                  error: false,
-                  message: message
-                });
-              }
-              else {
-                res.json({
-                  error: true,
-                  message: "Error: could not save user to proposal listening list "
-                });
-              }
-            });
-          } else {
-            res.json({ error: 'Proposal not found with this id' });
-          }
         })
-      } else {
-        res.json({ error: 'Not allowed to request' });
-      }
-    });
-  })
+    })
+
+
+/**
+     * @swagger
+     * /favorite_proposals:
+     *   post:
+     *     tags:
+     *       - FavoriteProposals
+     *     description: Favorite a proposals passed in query id to the user passed in the authorization
+     *     parameters:
+    *       - name: id
+    *         description: proposal id to favorite
+    *         in: query
+    *         required: true
+    *         type: string       
+    *       - name: Authorization
+    *         description: access token user
+    *         in: header
+    *         required: true
+    *         type: string
+     *     responses:
+     *       200:
+     *         description: Proposal favorited/unfavorited with success 
+     *       403:
+     *         description: invalid paramer
+     */
+router.route('/favorite_proposals')
+    .post(function(req, res) {
+        var token = req.headers.authorization
+        var result = false
+        var find = false
+        authvalidate.isValidToken(token, function(isValid) {
+            result = isValid
+            if (result) {
+                var id = req.query.id
+                Proposals.findOne({ id: id }, {}, function(err, result) {
+
+                    if (result != null) {
+                        // console.log(result.users)
+                        var message = " "
+                        var users = result.users
+
+                        if (users.length > 0) {
+                            users.forEach(function(obj, index) {
+                                console.log(index)
+                                if (token == obj) {
+                                    users.splice(index, 1);
+                                    message = "Unfavorited with success"
+                                    find = true
+                                    return
+                                }
+                            })
+                            if (!find) {
+                                users.push(token)
+                                message = "Favorited with success"
+                            }
+                        } else {
+                            users.push(token)
+                            message = "Favorited with success"
+                        }
+                        result.users = users
+                        result.save(function(err) {
+                            if (!err) {
+                                console.log(result.users)
+                                res.json({
+                                    error: false,
+                                    message: message
+                                });
+                            }
+                            else {
+                                res.json({
+                                    error: true,
+                                    message: "Error: could not save user to proposal listening list "
+                                });
+                            }
+                        });
+                    } else {
+                        res.json({ error: 'Proposal not found with this id' });
+                    }
+                })
+            } else {
+                res.json({ error: 'Not allowed to request' });
+            }
+        });
+    })
+
+
+    /**
+     * @swagger
+     * /favorite_proposals:
+     *   get:
+     *     tags:
+     *       - FavoriteProposals
+     *     description: Get all propouses favorited by the user passed in the authorization
+     *     parameters:  
+    *       - name: Authorization
+    *         description: access token user
+    *         in: header
+    *         required: true
+    *         type: string
+     *     responses:
+     *       200:
+     *         description: Proposals list of favorited user
+     *       403:
+     *         description: invalid user
+     */
+    .get(function(req, res) {
+        var token = req.headers.authorization
+        authvalidate.isValidToken(token, function(isValid) {
+
+            if (isValid) {
+
+                Proposals.find({ users: token }, function(err, result) {
+                    if (result != null) {
+
+                        let idsArray = result.map(function(x) {
+                            return x.id
+                        }).join()
+                        'WHERE topicos.id'
+                        let proposalsQuery = query + ' WHERE topicos.id IN (' + idsArray + ')'
+
+                        db.mysqlConnection.query(proposalsQuery, function(err, rows, fields) {
+                            if (!err) {
+                                res.json(rows);
+                            } else {
+                                res.send(err);
+                            }
+                        });
+                    } else {
+                        res.json({ error: err });
+                    }
+                })
+            } else {
+                res.json({ error: 'Not allowed to request' });
+            }
+        });
+    })
 
 module.exports = router;
